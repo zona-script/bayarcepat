71489b2f344ef6a99f2106d8f4980c7e088a7a6e
fatkulnurk <fatkulnurk@gmail.com>
2020-02-23 18:36:10 +0700

    update routing

--- routes/webCallback.php
+++ routes/webCallback.php
@@ -1,6 +1,6 @@
 <?php
 
 Route::group(['prefix' => 'callback-from-partner', 'namespace' => 'Callback'], function () {
-    Route::post('/javah2h', 'Javah2hController');
-    Route::get('/javah2h', 'Javah2hController');
+    Route::post('/javah2h', 'JavaH2HController');
+    Route::get('/javah2h', 'JavaH2HController');
 });
